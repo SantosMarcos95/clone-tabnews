9eb40b558fd921e575e52c381e7a454d31a893d1
Marcos Santos <94650963+SantosMarcos95@users.noreply.github.com>
2024-04-11 22:41:50 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() { 
+    return (
+    <>        
+        <h1> Hemilly, fiquei sabendo que você vai me dar um iphone de ultima geração novo, se for mentira, é so você dar um mortal para traz! </h1>)
+    </>
+ )
+}
+
+export default Home;
