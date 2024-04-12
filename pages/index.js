7f922b1f1eeb29fa1286bd2064cc7a80995954df
Marcos Santos <94650963+SantosMarcos95@users.noreply.github.com>
2024-04-12 23:46:33 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() { 
     return (
     <>        
-        <h1> Hemilly, fiquei sabendo que você vai me dar um iphone de ultima geração novo, se for mentira, é so você dar um mortal para traz! </h1>)
+        <h1> Hemilly, quer casar comigo? se sim, da um mortal para traz! </h1>)
     </>
  )
 }
