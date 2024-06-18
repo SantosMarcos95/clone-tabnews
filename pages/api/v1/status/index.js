7800512f93f78d9006b028d65a478b27bcbbe6f1
Marcos Santos <94650963+SantosMarcos95@users.noreply.github.com>
2024-06-18 20:09:09 +0000

    add `database.js` and `.env` files

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,8 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  response.status(200).json({ chave: "São acima da média" });
+}
+
+export default status;
